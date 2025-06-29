66487a7735c2ab09b0219e1757a25445fd3fb69a
Johndiddles <johnadepelumi@gmail.com>
2025-06-30 00:08:42 +0100

    minor patch

--- app/signin/error/page.tsx
+++ app/signin/error/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Suspense } from "react";
-import SignInErrorScreen from "@/components/SigninErrorPage/SigninErrorScreen";
+import SignInErrorScreen from "@/components/Screens/SigninErrorScreen";
 
 const SignInErrorPage = () => {
   return (
